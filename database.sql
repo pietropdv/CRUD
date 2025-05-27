2154912308a12ce2544a42c856eb2112546f8557
Aluno IFC <luizgennio31@gamil.com>
2025-05-27 11:27:48 -0300

    Versão com nova tabela

--- database.sql
+++ database.sql
@@ -15,11 +15,15 @@ CREATE TABLE categorias (
 );
 
 CREATE TABLE produtos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     descricao TEXT NOT NULL,
     preco DECIMAL(10,2) NOT NULL,
     quantidade INT NOT NULL,
     categoria INT NOT NULL,
     FOREIGN KEY (categoria) REFERENCES categorias(id)
+);
+
+CREATE TABLE pagamento(
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT NOT NULL,
 );
